e0bd1e9b26d00a7760f770b70a458998f9a1620a
Ilham Yori P <ilhamyoripradana@gmail.com>
2025-03-18 20:39:00 +0700

    Adding Matrix Checker

--- src/Main.java
+++ src/Main.java
@@ -29,5 +29,41 @@ public class Main {
                 }
             }
         }
+
+        for (int y = 1; y < height - 1; y++) {
+            for (int x = 1; x < length - 1; x++) {
+                if (matrix[y][x] == 1 &&
+                        matrix[y - 1][x] == 1 &&
+                        matrix[y + 1][x] == 1 &&
+                        matrix[y][x - 1] == 1 &&
+                        matrix[y][x + 1] == 1) {
+
+                    if (marked[y][x] ||
+                            marked[y - 1][x] ||
+                            marked[y + 1][x] ||
+                            marked[y][x - 1] ||
+                            marked[y][x + 1]) {
+
+                        continue;
+                    }
+
+                    display[y][x] = '*';
+                    display[y - 1][x] = '*';
+                    display[y + 1][x] = '*';
+                    display[y][x - 1] = '*';
+                    display[y][x + 1] = '*';
+
+                    marked[y][x] = true;
+                    marked[y - 1][x] = true;
+                    marked[y + 1][x] = true;
+                    marked[y][x - 1] = true;
+                    marked[y][x + 1] = true;
+
+                    crossCenters.add("(" + x + ", " + y + ")");
+                }
+            }
+        }
+
+
     }
 }
